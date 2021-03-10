a4c6869830218f387509cbd904b22cf2e1547872
Jordan Pillai <jordan.pillai@steel-eye.com>
2021-03-10 08:29:02 +0000

    Refactor formatProductArray

--- utils/index.js
+++ utils/index.js
@@ -2,27 +2,28 @@ export const formatNumber = (number) =>
   new Intl.NumberFormat('en', { minimumFractionDigits: 2 }).format(number);
 
 export const formatProductArray = (data) => {
-  const flatten = (arr) => [].concat(...arr);
-  const productArray = flatten(Array.from(data, (array) => array.products));
-  const results = [];
-  const obj = {};
-
-  productArray.forEach(({ name, unitPrice, sold = 0 }) => {
-    if (!obj.name) {
-      obj.name = {
-        name,
-        unitPrice,
-        sold,
-      };
-      results.push(obj.name);
-    }
-    obj.name.sold += sold;
-  }, obj);
-
-  return results.sort((a, b) =>
-    a.name > b.name ? 1 : b.name > a.name ? -1 : 0
+  const productArray = data.reduce(
+    (acc, current) => [...acc, ...current.products],
+    []
   );
+
+  const results = productArray.reduce((acc, current) => {
+    const duplicate = acc.find((item) => item.name === current.name);
+
+    if (duplicate) {
+      return acc.map((product) =>
+        product === duplicate
+          ? { ...duplicate, sold: product.sold + current.sold }
+          : product
+      );
+    }
+
+    return [...acc, current];
+  }, []);
+
+  return results.sort((a, b) => a.name.localeCompare(b.name));
 };
 
 export const calculateTotal = (arr) =>
   arr.reduce((sum, { unitPrice, sold }) => sum + unitPrice * sold, 0);
+
